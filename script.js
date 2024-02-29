71f28b7cc264473bdfc2b715ee28b63d6184c159
Quinton Chinouya <quintonsmylie@yahoo.com>
2024-02-28 18:43:12 -0600

    added a set alarm button and dropdown menu. also added the logic behind it

---
+++ script.js
@@ -0,0 +1,57 @@
+function updateTime() {
+    var now = new Date();
+    var hours = now.getHours();
+    var meridiem = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    var minutes = String(now.getMinutes()).padStart(2, '0');
+    var seconds = String(now.getSeconds()).padStart(2, '0');
+    
+    var timeString = hours + ":" + minutes + ":" + seconds + " " + meridiem;
+    
+    document.getElementById("time").innerHTML = timeString;
+  }
+  
+function populateDropdowns(){
+    var hourSelect = document.getElementById("hourSelect");
+    var minuteSelect = document.getElementById("minuteSelect");
+    var secondSelect = document.getElementById("secondSelect");
+    var meridiemSelect = document.getElementById("meridiemSelect");
+
+    for (var i = 0; i < 12; i++) {
+        var option = document.createElement("option");
+        option.text = i.toString().padStart(2, 0);
+        hourSelect.add(option);
+    }
+
+    for (var i = 0; i < 60; i++) {
+        var option = document.createElement("option");
+        option.text = i.toString().padStart(2, 0);
+        minuteSelect.add(option);
+        secondSelect.add(option.cloneNode(true));
+    }
+
+    var option = document.createElement("option");
+    option.text = "am";
+    meridiemSelect.add(option);
+
+    var option1 = document.createElement("option1");
+    option1.text = "pm";
+    meridiemSelect.add(option1);
+  
+}
+function setAlarm () {
+    var hour = document.getElementById("hourSelect").value;
+    var minute = document.getElementById("minuteSelect").value;
+    var second = document.getElementById("secondSelect").value;
+    var meridiem = document.getElementById("meridiemSelect").value;
+
+    var alarmTime = hour + ":" + minute + ":" + second + " " + meridiem;
+
+    console.log("Alarm set for: " + alarmTime);
+}
+ 
+  setInterval(updateTime, 1000);
+
+  updateTime();
+  window.onload = populateDropdowns;
